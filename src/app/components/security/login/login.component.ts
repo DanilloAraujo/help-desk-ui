1978e00eafeb8b6a31b6ab7bd7da626e12490c4d
Danillo Araujo Silva <dsilva@scopus.com.br>
2018-03-26 10:22:08 -0300

    Criando Componente Login Parte 01

--- src/app/components/security/login/login.component.ts
+++ src/app/components/security/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from './../../../model/user.model';
+import { CurrentUser } from '../../../model/current-user.model';
+
+import { SharedService } from '../../../services/shared.service';
+import { UserService } from './../../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user = new User(null, '', '', '');
+  shared: SharedService;
+  message: string;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { 
+    this.shared = SharedService.getInstance();
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.message = '';
+    this.userService.login(this.user).subscribe((userAuthentication: CurrentUser) => {
+      this.shared.token = userAuthentication.token;
+      this.shared.user = userAuthentication.user;
+      this.shared.user.profile = this.shared.user.profile.substring(5);
+      this.shared.showTemplate.emit(true);
+      this.router.navigate(['/']);
+    }, err => {
+      this.shared.token = null;
+      this.shared.user = null;
+      this.shared.showTemplate.emit(false);
+      this.message = 'Error';
+    });
+  }
+
+  cancelLogin() {
+    this.message = '';
+    this.user = new User(null, '', '', '');
+    window.location.href = '/login';
+    window.location.reload();
   }
 
 }
