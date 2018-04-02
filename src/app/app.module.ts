a80d4645ff05e7e19635a2973843c2368c27c926
Danillo Araujo Silva <dsilva@scopus.com.br>
2018-04-02 16:27:49 -0300

    Criando Componente Summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { SharedService } from './services/shared.service';
 import { TicketNewComponent } from './components/ticket-new/ticket-new.component';
 import { TicketListComponent } from './components/ticket-list/ticket-list.component';
 import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.
     UserListComponent,
     TicketNewComponent,
     TicketListComponent,
-    TicketDetailComponent
+    TicketDetailComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { TicketNewComponent } from './components/ticket-new/ticket-new.component
 import { UserListComponent } from './components/user-list/user-list.component';
 import { TicketListComponent } from './components/ticket-list/ticket-list.component';
 import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
+import { SummaryComponent } from './components/summary/summary.component';
 import { AuthGuard } from './components/security/auth.guard';
 
 export const ROUTES: Routes = [
@@ -19,7 +20,8 @@ export const ROUTES: Routes = [
     {path: 'ticket-new', component: TicketNewComponent, canActivate: [AuthGuard]},
     {path: 'ticket-new/:id', component: TicketNewComponent, canActivate: [AuthGuard]},
     {path: 'ticket-list', component: TicketListComponent, canActivate: [AuthGuard]},
-    {path: 'ticket-detail/:id', component: TicketDetailComponent, canActivate: [AuthGuard]}
+    {path: 'ticket-detail/:id', component: TicketDetailComponent, canActivate: [AuthGuard]},
+    {path: 'summary', component: SummaryComponent, canActivate: [AuthGuard]}
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
