de4d33c701dea40c9d58a588c94a50439f3d020f
Danillo Araujo Silva <dsilva@scopus.com.br>
2018-04-02 15:20:45 -0300

    Criando Componente TicketDetail Parte 02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { UserNewComponent } from './components/user-new/user-new.component';
 import { TicketNewComponent } from './components/ticket-new/ticket-new.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { TicketListComponent } from './components/ticket-list/ticket-list.component';
+import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
 import { AuthGuard } from './components/security/auth.guard';
 
 export const ROUTES: Routes = [
@@ -17,7 +18,8 @@ export const ROUTES: Routes = [
     {path: 'user-list', component: UserListComponent, canActivate: [AuthGuard]},
     {path: 'ticket-new', component: TicketNewComponent, canActivate: [AuthGuard]},
     {path: 'ticket-new/:id', component: TicketNewComponent, canActivate: [AuthGuard]},
-    {path: 'ticket-list', component: TicketListComponent, canActivate: [AuthGuard]}
+    {path: 'ticket-list', component: TicketListComponent, canActivate: [AuthGuard]},
+    {path: 'ticket-detail/:id', component: TicketDetailComponent, canActivate: [AuthGuard]}
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
